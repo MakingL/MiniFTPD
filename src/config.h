00b31271fc2c6ba2ea26eb2b508e68ea77c51cc7
MichaelLee <lj1748503358@163.com>
2020-01-08 20:46:30 +0800

    Add: Version

---
+++ src/config.h
@@ -0,0 +1,16 @@
+//
+// Created by MLee on 2020/1/03.
+//
+
+#ifndef MINIFTPD_CONFIG_H
+#define MINIFTPD_CONFIG_H
+
+// 版本号信息
+#define MINIFTPD_VERSION_MAJOR 0
+#define MiniFTPD_VERSION_MINOR 1
+
+// 配置文件所在路径
+#define CONFIG_FILE_PATH "config.yaml"
+
+
+#endif //MINIFTPD_CONFIG_H
